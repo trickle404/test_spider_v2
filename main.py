9afebacd38ef4244c3cb4e65ad2ee301d295943e
trickle404 <m.derkaqw@gmail.com>
2025-07-28 16:28:17 +0200

    Add Crawler Process

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from parse_data.spiders.proxies import ProxiesSpyder
+from parse_data.spiders.sender import SenderSpider
 
 def main():
     proccess = CrawlerProcess(get_project_settings())
     proccess.crawl(ProxiesSpyder)
+    proccess.crawl(SenderSpider)
 
 if __name__ == "__main__":
     main()
